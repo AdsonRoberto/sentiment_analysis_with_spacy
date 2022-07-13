397c9d9ef39f5fc851174406a3d38f187690d35c
AdsonRoberto <adson.uece@gmail.com>
2022-07-13 15:08:27 -0300

    pre-processando os textos

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -17,3 +17,29 @@ base_dados.shape
 base_dados.head()
 base_dados.tail()
 sns.countplot(base_dados['emocao'], label = 'Contagem')
+
+pontuacoes = string.punctuation
+pontuacoes
+
+from spacy.lang.pt.stop_words import STOP_WORDS
+stop_words = STOP_WORDS
+
+print(stop_words)
+
+len(stop_words)
+
+pln = spacy.load('pt')
+
+def preprocessamento(texto):
+  texto = texto.lower()
+  documento = pln(texto)
+  
+  lista = []
+  for token in documento:
+    #lista.append(token.text)
+    lista.append(token.lemma_)
+
+  lista = [palavra for palavra in lista if palavra not in stop_words and palavra not in pontuacoes]
+  lista = ' '.join([str(elemento) for elemento in lista if not elemento.isdigit()])
+
+  return lista
